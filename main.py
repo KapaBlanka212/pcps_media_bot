c0dfe5b0be985c0294899d41c0f5d629977489e4
NikKostromin <buchadefred@gmail.com>
2024-08-28 15:18:46 +0300

    feat(user restriction)

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 """
 Developed with love by Nick(@NikiKostr) for PCPS Media telegramm group
 """
-import logging
 import os
 from dotenv import load_dotenv
 from telegram.ext import Application, MessageHandler, CommandHandler
 import telegram.ext.filters as filters
 
 from method import restricted_access, start, send_to_chat_group, handle_text, get_chat_and_thread_id
-
+from logger_control import setup_logger
 """
 UNIT: TOKENS
 ~~~~~~~~~~~~
 """
-
+loger = setup_logger("main", "bot.log")
 load_dotenv()
 TOKEN = os.getenv('YOUR_TOKEN')
-
 
 def main() -> None:
     application = Application.builder().token(TOKEN).build()
@@ -26,12 +24,10 @@ def main() -> None:
     application.add_handler(CommandHandler("get_chat_and_thread_id", restricted_access(get_chat_and_thread_id)))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text))
 
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-
     try:
         application.run_polling()
     except Exception as e:
-        logging.error(f"An error occurred: {e}")
+        loger.error(f"An error occurred: {e}")
 
 if __name__ == '__main__':
     main()
--- method.py
+++ method.py
@@ -1,4 +1,6 @@
+import os
 import re
+from dotenv import load_dotenv
 from telegram import Update
 from telegram.constants import ParseMode
 from telegram.ext import CallbackContext
@@ -6,22 +8,30 @@ from telegram.ext import CallbackContext
 import constnats as CONST
 from logger_control import setup_logger
 
-"""
-UNIT: FUCTION
-~~~~~~~~~~~~~
-    In block of code bellow are described all nessessary functions for Polina tg bot.
-"""
-
 logger = setup_logger(CONST.METHODS_LOGGER_NAME, CONST.METHODS_LOGGER_FILE)
+# Загрузка переменных окружения
+load_dotenv()
+
+# Получение списка разрешенных пользователей из переменной окружения и удаление символа '@'
+allowed_users = os.getenv('ALLOWED_USERS').split(',')
+
+def restricted_access(func):
+    async def wrapper(update: Update, context: CallbackContext) -> None:
+        user = update.message.from_user
+        username = user.username
+        logger.info(username)
+        if username not in allowed_users:
+            await update.message.reply_text(CONST.MSG_NO_ACCESS)
+            return
+        return await func(update, context)
+    return wrapper
 
 async def handle_text(update: Update, context: CallbackContext) -> None:
     user_text = update.message.text
     await update.message.reply_text(f"Вы написали: {user_text}")
 
-
 async def start(update: Update, context: CallbackContext) -> None:
     await update.message.reply_text("Привет! Я новый и очень крутой бот-анонсер для чата медиа отдела PCPS.")
-
 
 def sanitize_input(input_text: str) -> str:
     sanitized_text = re.sub(r'[^\w\s@]', '', input_text)
@@ -31,7 +41,6 @@ def escape_markdown(text):
     escape_chars = r'\*_`['
     return ''.join(f'\\{char}' if char in escape_chars else char for char in text)
 
-# Асинхронный метод для отправки сообщения в группу
 async def send_to_chat_group(update: Update, context: CallbackContext) -> None:
     chat_id = update.message.chat_id
     message_thread_id = update.message.message_thread_id
@@ -39,7 +48,7 @@ async def send_to_chat_group(update: Update, context: CallbackContext) -> None:
     message = sanitize_input(' '.join(context.args[1:]))
     
     if group_name in CONST.GROUPS:
-        tagged_users = ' '.join([f'{user}' for user in CONST.GROUPS[group_name]])
+        tagged_users = ' '.join([f'@{user}' for user in CONST.GROUPS[group_name]])
         full_message = escape_markdown(f'{message} {tagged_users}')
         
         if message_thread_id:
@@ -49,19 +58,7 @@ async def send_to_chat_group(update: Update, context: CallbackContext) -> None:
     else:
         await update.message.reply_text(CONST.MSG_GROUP_NOT_EXIST.format(group_name=group_name))
 
-
 async def get_chat_and_thread_id(update: Update, context: CallbackContext) -> None:
     chat_id = update.message.chat_id
     message_thread_id = update.message.message_thread_id
     await update.message.reply_text(f"Chat ID: {chat_id}\nThread ID: {message_thread_id}")
-
-
-def restricted_access(func):
-    async def wrapper(update: Update, context: CallbackContext) -> None:
-        allowed_users = ['NIKI']
-        user = update.message.from_user
-        if user.username not in allowed_users:
-            await update.message.reply_text(CONST.MSG_NO_ACCESS)
-            return
-        return await func(update, context)
-    return wrapper
